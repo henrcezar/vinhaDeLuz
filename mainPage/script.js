fb718d627242bb9894769100f4197705aab4cf30
Henrique Cézar Ferreira de Oliveira <98775810+henrcezar@users.noreply.github.com>
2024-03-10 18:58:59 +0000

    Sita Vinha 2.0

--- mainPage/script.js
+++ mainPage/script.js
@@ -1,7 +1,23 @@
-let elementsOptions = document.querySelectorAll('.options')
+//Ride elements Function
+let elementsOptions = document.querySelectorAll(".options")
 
 elementsOptions.forEach(function (options) {
-    options.addEventListener('click', function (){
-        options.classList.toggle('active')
-    })
+  options.addEventListener("click", function () {
+    options.classList.toggle("active")
+  })
 })
+
+//Modal Function Agenda
+let modal = document.getElementById("myModal")
+let img = document.getElementById("myImg")
+let modalImg = document.getElementById("img01")
+let captionText = document.getElementById("caption")
+img.onclick = function () {
+  modal.style.display = "block"
+  modalImg.src = this.src
+  captionText.innerHTML = this.alt
+}
+let span = document.getElementsByClassName("close")[0]
+span.onclick = function () {
+  modal.style.display = "none"
+}
